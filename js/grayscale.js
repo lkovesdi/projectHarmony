6c3fb341dae129b49ab352b439c4064d84a806b1
Laszlo Kovesdi <lacikovesdi@gmail.com>
2018-05-16 09:58:03 -0700

    fixes

--- js/grayscale.js
+++ js/grayscale.js
@@ -1,3 +1,26 @@
+$(function(){
+	$('.b-slogan__button-play').magnificPopup({
+		//delegate: '.b-slogan__url', // child items selector, by clicking on it popup will open
+		type: 'iframe',
+		tLoading: 'Loading video #%curr%...',
+		modal:false,
+		markup: '<div class="mfp-iframe-scaler">'+
+            '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>'+
+          '</div>', 
+		callbacks:{
+			open: function(){
+				$('.mfp-close').remove();//удаление старой кнопки
+				$('<button title="" type="button" class="mfp-close"></button>').appendTo('.mfp-container');
+				//var content = $(".mfp-iframe");
+			}
+		}
+	});
+});
+
+
+
+
+
 (function($) {
   "use strict"; // Start of use strict
 
@@ -192,3 +215,6 @@ function init() {
     icon: image
   });
 }
+
+
+
